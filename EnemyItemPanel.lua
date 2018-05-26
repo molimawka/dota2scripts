19422fe0fde16f8f85e1b90b4dc706b5517256af
molimawka <38759039+molimawka@users.noreply.github.com>
2018-05-27 02:59:20 +0300

    Update EnemyItemPanel.lua

--- EnemyItemPanel.lua
+++ EnemyItemPanel.lua
@@ -4,14 +4,15 @@ enemyItemPanel.enable = Menu.AddOptionBool({"Awareness", "EnemyItemPanel"}, "Ene
 enemyItemPanel.key = Menu.AddKeyOption({"Awareness", "EnemyItemPanel"}, "EnemyItemPanel Key", Enum.ButtonCode.KEY_SPACE)
 enemyItemPanel.x =  Menu.AddOptionSlider({"Awareness", "EnemyItemPanel"}, "EnemyItemPanel x", 0, 1920, 0)
 enemyItemPanel.y =  Menu.AddOptionSlider({"Awareness", "EnemyItemPanel"}, "EnemyItemPanel y", 0, 1080, 0)
-enemyItemPanel.IconSize =  Menu.AddOptionSlider({"Awareness", "EnemyItemPanel"}, "Icon Size", 24, 48, 32)
-enemyItemPanel.IntRColor =  Menu.AddOptionSlider({"Awareness", "EnemyItemPanel"}, "R Color", 0, 255, 255)
-enemyItemPanel.IntGColor =  Menu.AddOptionSlider({"Awareness", "EnemyItemPanel"}, "G Color", 0, 255, 255)
-enemyItemPanel.IntBColor =  Menu.AddOptionSlider({"Awareness", "EnemyItemPanel"}, "B Color", 0, 255, 255)
+enemyItemPanel.IconSize =  Menu.AddOptionSlider({"Awareness", "EnemyItemPanel"}, "Icon Size Panel", 24, 48, 32)
+enemyItemPanel.enableHero = Menu.AddOptionBool({"Awareness", "EnemyItemPanel"}, "ItemPanel over Hero Enable", false)
+enemyItemPanel.yPosWithHero =  Menu.AddOptionSlider({"Awareness", "EnemyItemPanel"}, "Y Position with Hero", -300, 10, -180)
+enemyItemPanel.IconSizeOverHero =  Menu.AddOptionSlider({"Awareness", "EnemyItemPanel"}, "Icon Size over Hero", 24, 64, 32)
 enemyItemPanel.font = Renderer.LoadFont("Tahoma", 20, Enum.FontWeight.EXTRABOLD)
 
 local draw = true;
 local item = {}
+local itemVal = {}
 local imgItem = {}
 local imgHero = {}
 
@@ -30,23 +31,25 @@ function enemyItemPanel.Work(myHero)
 		if Entity.IsSameTeam(myHero, h) or NPC.IsIllusion(h) then return end
 		local heroName = string.sub(tostring(NPC.GetUnitName(h)),string.len("npc_dota_hero_") + 1)
 		local cachedItems = {}
+		itemVal[heroName] = 1
 		for p = 0, 8 do
 			local itemName
 			if Entity.IsEntity(NPC.GetItemByIndex(h, p)) then
 				itemName = tostring(Ability.GetName(NPC.GetItemByIndex(h, p)))
+				itemVal[heroName] = itemVal[heroName]+1
 			else
 				itemName = "item_null"
 			end
 			table.insert(cachedItems, string.sub(itemName ,string.len("item_") + 1))
 		end
 		item[heroName] = cachedItems
+		draw = false
 	end
-	draw = false
 end
 
 function enemyItemPanel.OnDraw()
-	local r, g, b = Menu.GetValue(enemyItemPanel.IntRColor), Menu.GetValue(enemyItemPanel.IntGColor), Menu.GetValue(enemyItemPanel.IntBColor)
 	local IconSize = Menu.GetValue(enemyItemPanel.IconSize)
+	local IconSizeOverHero = Menu.GetValue(enemyItemPanel.IconSizeOverHero)
 	local myHero = Heroes.GetLocal()
 	if not Menu.IsEnabled(enemyItemPanel.enable) then return end
 	if draw then return end
@@ -58,9 +61,11 @@ function enemyItemPanel.OnDraw()
 		Menu.SetValue(enemyItemPanel.x, x)
 		Menu.SetValue(enemyItemPanel.y, y)
 	end
+	local x1, y1;
 	local heroes = Heroes.GetAll()
 	for i, h in ipairs(heroes) do
 		if Entity.IsSameTeam(myHero, h) or NPC.IsIllusion(h) then return end
+		if NPC.IsVisible(h) then x1, y1 = Renderer.WorldToScreen(Entity.GetAbsOrigin(h)) end
 		local heroName = string.sub(tostring(NPC.GetUnitName(h)),string.len("npc_dota_hero_") + 1)
 		local temp
 		if imgHero[heroName] == nil then
@@ -77,13 +82,27 @@ function enemyItemPanel.OnDraw()
 				end
 				temp = imgItem[i]
 				Renderer.SetDrawColor(255, 255, 255, 255)
+				if math.floor(Ability.GetCooldown(NPC.GetItemByIndex(h, o-1))) > 0 then
+					Renderer.SetDrawColor(165, 165, 165, 255)
+				end
 				Renderer.DrawImage(temp, x, y, IconSize, IconSize)
+				local cooldown
 				if Entity.IsEntity(NPC.GetItemByIndex(h, o-1)) then
-					local cooldown = tostring(math.floor(Ability.GetCooldown(NPC.GetItemByIndex(h, o-1))))
-					Renderer.SetDrawColor(r, g, b, 255)
+					cooldown = tostring(math.floor(Ability.GetCooldown(NPC.GetItemByIndex(h, o-1))))
+					Renderer.SetDrawColor(255, 255, 255, 255)
 					Renderer.DrawText(enemyItemPanel.font, x, y, cooldown)
 				end
 				x = x+IconSize
+				if NPC.IsVisible(h) and Menu.IsEnabled(enemyItemPanel.enableHero) then
+					Renderer.SetDrawColor(255, 255, 255, 255)
+					if Entity.IsEntity(NPC.GetItemByIndex(h, o-1)) and math.floor(Ability.GetCooldown(NPC.GetItemByIndex(h, o-1))) > 0 then
+						Renderer.SetDrawColor(165, 165, 165, 255)
+					end
+					Renderer.DrawImage(temp, x1-(IconSizeOverHero*(itemVal[heroName]/2)), y1+Menu.GetValue(enemyItemPanel.yPosWithHero), IconSizeOverHero, IconSizeOverHero)
+					Renderer.SetDrawColor(255, 255, 255, 255)
+					Renderer.DrawText(enemyItemPanel.font, x1-(IconSizeOverHero*(itemVal[heroName]/2)), y1+Menu.GetValue(enemyItemPanel.yPosWithHero), cooldown)
+					x1 = x1+IconSizeOverHero
+				end
 			end
 		end
 		x = Menu.GetValue(enemyItemPanel.x)
