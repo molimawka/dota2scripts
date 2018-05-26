c200db0eb809d375f183419441b0d6276b95b7de
molimawka <38759039+molimawka@users.noreply.github.com>
2018-05-26 22:20:57 +0300

    Update EnemyItemPanel.lua

--- EnemyItemPanel.lua
+++ EnemyItemPanel.lua
@@ -4,6 +4,11 @@ enemyItemPanel.enable = Menu.AddOptionBool({"Awareness", "EnemyItemPanel"}, "Ene
 enemyItemPanel.key = Menu.AddKeyOption({"Awareness", "EnemyItemPanel"}, "EnemyItemPanel Key", Enum.ButtonCode.KEY_SPACE)
 enemyItemPanel.x =  Menu.AddOptionSlider({"Awareness", "EnemyItemPanel"}, "EnemyItemPanel x", 0, 1920, 0)
 enemyItemPanel.y =  Menu.AddOptionSlider({"Awareness", "EnemyItemPanel"}, "EnemyItemPanel y", 0, 1080, 0)
+enemyItemPanel.IconSize =  Menu.AddOptionSlider({"Awareness", "EnemyItemPanel"}, "Icon Size", 24, 48, 32)
+enemyItemPanel.IntRColor =  Menu.AddOptionSlider({"Awareness", "EnemyItemPanel"}, "R Color", 0, 255, 255)
+enemyItemPanel.IntGColor =  Menu.AddOptionSlider({"Awareness", "EnemyItemPanel"}, "G Color", 0, 255, 255)
+enemyItemPanel.IntBColor =  Menu.AddOptionSlider({"Awareness", "EnemyItemPanel"}, "B Color", 0, 255, 255)
+enemyItemPanel.font = Renderer.LoadFont("Tahoma", 20, Enum.FontWeight.EXTRABOLD)
 
 local draw = true;
 local item = {}
@@ -40,6 +45,8 @@ function enemyItemPanel.Work(myHero)
 end
 
 function enemyItemPanel.OnDraw()
+	local r, g, b = Menu.GetValue(enemyItemPanel.IntRColor), Menu.GetValue(enemyItemPanel.IntGColor), Menu.GetValue(enemyItemPanel.IntBColor)
+	local IconSize = Menu.GetValue(enemyItemPanel.IconSize)
 	local myHero = Heroes.GetLocal()
 	if not Menu.IsEnabled(enemyItemPanel.enable) then return end
 	if draw then return end
@@ -61,20 +68,26 @@ function enemyItemPanel.OnDraw()
 		end
 		temp = imgHero[heroName]
 		Renderer.SetDrawColor(255, 255, 255, 255)
-		Renderer.DrawImage(temp, x, y, 24, 24)
-		x = x+24
+		Renderer.DrawImage(temp, x, y, IconSize, IconSize)
+		x = x+IconSize
 		for o, i in ipairs(item[heroName]) do
 			if i ~= "null" then
 				if imgItem[i] == nil then
 					imgItem[i] = Renderer.LoadImage("~/CustomUI/items/"..i..".png");
 				end
 				temp = imgItem[i]
-				Renderer.DrawImage(temp, x, y, 24, 24)
-				x = x+24
+				Renderer.SetDrawColor(255, 255, 255, 255)
+				Renderer.DrawImage(temp, x, y, IconSize, IconSize)
+				if Entity.IsEntity(NPC.GetItemByIndex(h, o-1)) then
+					local cooldown = tostring(math.floor(Ability.GetCooldown(NPC.GetItemByIndex(h, o-1))))
+					Renderer.SetDrawColor(r, g, b, 255)
+					Renderer.DrawText(enemyItemPanel.font, x, y, cooldown)
+				end
+				x = x+IconSize
 			end
 		end
 		x = Menu.GetValue(enemyItemPanel.x)
-		y = y+24
+		y = y+IconSize
 	end
 end
 
